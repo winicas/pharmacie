978d8bd2edf3d04e17266e40b413e4ab7dcaa59e
winica <winicaloseka@gmail.com>
2025-06-19 17:37:59 +0100

    molimo

--- hopitalsage_front/app/dashboard/pharmacie/nouveau-lot/[id]/page.tsx
+++ hopitalsage_front/app/dashboard/pharmacie/nouveau-lot/[id]/page.tsx
@@ -1,12 +1,11 @@
 // app/dashboard/pharmacie/nouveau-lot/[id]/page.tsx
 
 export default function Page({ params }: { params: { id: string } }) {
-  const { id } = params;
+  const id = params.id
 
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold">Modifier le lot {id}</h1>
-      {/* Ton formulaire ou affichage ici */}
     </div>
-  );
+  )
 }
