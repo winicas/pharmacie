49a9e528dca0c7df89ac0a0882c8ee0077684ab5
winica <winicaloseka@gmail.com>
2025-07-29 15:56:00 +0100

    adminusehhhjjkr

--- gestion_pharmacie/settings.py
+++ gestion_pharmacie/settings.py
@@ -9,15 +9,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-#logging.basicConfig(level=logging.DEBUG) 
+logging.basicConfig(level=logging.DEBUG) 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#z+!08_7q)g9zgor&1vk+@l8x)$5fp&4-!a#r^+p@)k3khf#-m'
-#SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = 'django-insecure-#z+!08_7q)g9zgor&1vk+@l8x)$5fp&4-!a#r^+p@)k3khf#-m'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-#ALLOWED_HOSTS = ["pharmacie-hefk.onrender.com"]
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["pharmacie-hefk.onrender.com"]
+#ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -118,11 +118,11 @@ from decouple import config
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_DB'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),
-        'HOST': config('POSTGRES_HOST', default='localhost'),
-        'PORT': config('POSTGRES_PORT', default='5432'),
+        'NAME': 'nicapharm',
+        'USER': 'nicapharm_user',
+        'PASSWORD': 'WTIVUA2bEab4gowGJyNZ0WRKY5g6tXq8',
+        'HOST': 'dpg-d1t3j93e5dus73e4am20-a',
+        'PORT': '5432',
     },
     'remote': 
     {
