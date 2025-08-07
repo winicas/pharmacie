c5e3a9c8d181841cfc9867ce8a644da72a7bf693
winica <winicaloseka@gmail.com>
2025-08-07 11:00:39 +0100

    molimo santu sur moi william

--- gestion_pharmacie/settings.py
+++ gestion_pharmacie/settings.py
@@ -9,15 +9,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-#logging.basicConfig(level=logging.DEBUG) 
+logging.basicConfig(level=logging.DEBUG) 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#z+!08_7q)g9zgor&1vk+@l8x)$5fp&4-!a#r^+p@)k3khf#-m'
-#SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = 'django-insecure-#z+!08_7q)g9zgor&1vk+@l8x)$5fp&4-!a#r^+p@)k3khf#-m'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-#ALLOWED_HOSTS = ["pharmacie-hefk.onrender.com"]
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["pharmacie-hefk.onrender.com"]
+#ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
